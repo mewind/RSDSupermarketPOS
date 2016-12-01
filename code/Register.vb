794f473e54dfd3b1934f2aea1c18742d43f73c91
huylin25 <hyelin1125@hotmail.com>
2016-12-02 00:15:28 +0800

    [#135340245][#135340269][#134240845] Updated StaffID & MemID validation
    Update Included :
    - Verify staffID if user type selected is staff
    -Verify MemberID if user type selected is member
    - Create a new user after all the validation is done into database

--- code/Register.vb
+++ code/Register.vb
@@ -153,5 +153,136 @@
             err.SetError(cboCardCompany, Nothing)
         End If
     End Sub
+    'check whther STAFFID is empty when STAFFS is selected in the cbox
+    Private Sub txtStaffID_Validating(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles txtStaffID.Validating
+        If cboUserType.Text = "Staff" Then
+            Dim staffid As String = If(txtStaffID.Text, " ")
+
+            If staffid = "" Then
+                err.SetError(txtStaffID, "Please enter your staff ID")
+                e.Cancel = True
+
+            Else
+                err.SetError(txtStaffID, Nothing)
+            End If
+        End If
+
+
+    End Sub
+
+    'check whether MEMBER ID is empty when MEMBERS is selected in the cbox
+    Private Sub txtMemberID_Validating(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles txtMemberID.Validating
+
+        If cboUserType.Text = "Member" Then
+            Dim memberid As String = If(txtMemberID.Text, " ")
+
+            If memberid = "" Then
+                err.SetError(txtMemberID, "Please enter your member ID")
+                e.Cancel = True
+
+
+            Else
+                err.SetError(txtMemberID, Nothing)
+            End If
+
+
+        End If
+
+
+    End Sub
+    '<----------------VALIDATING --------------------------->
+
+
+    'VALIDATE STAFF ID
+    Private Function ValidateStaff(staffid As String) As Boolean
+        Dim db As New DBDataContext()
+
+        Dim staff As Staff = db.Staffs.SingleOrDefault(Function(s) s.staff_id = staffid)
+
+        Return (staff IsNot Nothing)
+    End Function
+
+    'VALIDATE MEMBER ID
+    Private Function ValidateMember(memberid As String) As Boolean
+        Dim db As New DBDataContext()
+
+        Dim member As Member = db.Members.SingleOrDefault(Function(m) m.member_id = memberid)
+
+        Return (member IsNot Nothing)
+    End Function
+
+    'btnRegister Clicked  
+    Private Sub btnRegister_Click(sender As Object, e As EventArgs) Handles btnRegister.Click
+        'FORMAT
+        btnRegister.FlatAppearance.BorderColor = Color.Black
+        btnRegister.FlatAppearance.BorderSize = 2
+
+
+        Dim ismember As Boolean = ValidateMember(txtMemberID.Text)
+        If ismember = False Then
+            err.SetError(txtMemberID, "You've entered the wrong MemberID!")
+        Else
+            err.SetError(txtMemberID, Nothing)
+        End If
+
+        Dim isstaff As Boolean = ValidateStaff(txtStaffID.Text)
+        If isstaff = False Then
+            err.SetError(txtStaffID, "You've entered the wrong StaffID!")
+        Else
+            err.SetError(txtStaffID, Nothing)
+        End If
+
+
+        If Me.ValidateChildren() = False Then
+            Return
+        End If
+
+        Dim db As New DBDataContext()
+        Dim user As New User
+
+        'Insert new user
+        user.user_id = "RSD000" & (db.Users.Count() + 1).ToString
+
+        user.user_name = txtUsername.Text
+        user.user_password = txtPassword.Text
+        user.user_email = txtEmail.Text
+        If cboCardType.SelectedItem = "Debit Card" Then
+            user.user_cardtype = "Debit"
+        ElseIf cboCardType.SelectedItem = "Credit Card" Then
+            user.user_cardtype = "Credit"
+        End If
+        user.user_ccompany = ""
+        user.user_CCV = ""
+        user.staff_id = ""
+        user.member_id = ""
+        user.user_ccompany = cboCardCompany.Text
+        user.user_CCV = txtCCV.Text
+
+        If cboUserType.SelectedItem = "Member" Then
+
+            user.member_id = txtMemberID.Text
+            user.staff_id = Nothing
+        ElseIf cboUserType.SelectedItem = "Staff" Then
+            user.staff_id = txtStaffID.Text
+            user.staff_id = Nothing
+        ElseIf cboUserType.SelectedItem = "Non-member" Then
+            user.staff_id = Nothing
+            user.member_id = Nothing
+        End If
+
+        db.Users.InsertOnSubmit(user)
+        db.SubmitChanges()
+
+        If btnRegister.Enabled = True Then
+            MessageBox.Show("Congratulations! You have registered successfully!", "Notice", MessageBoxButtons.OK, MessageBoxIcon.None)
+
+            frmUserLogin.Show()
+            Me.Hide()
+
+        End If
+
+
+    End Sub
+
 
 End Class
