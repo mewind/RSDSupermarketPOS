f248ad58c010b27bb6620ebeb9cb6f9daca43a53
huylin25 <hyelin1125@hotmail.com>
2016-12-02 00:11:12 +0800

    [#135339633] Update check empty
    Update Included:
    - Check whether the fields is empty
    - Call the Isduplicated() function coded before to apply the function

--- code/Register.vb
+++ code/Register.vb
@@ -53,4 +53,105 @@
         End If
     End Function
 
+    '<----------------VALIDATING --------------------------->
+    'Check whther txtUsername is empty 
+    Private Sub txtUsername_Validating(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles txtUsername.Validating
+        Dim username As String = If(txtUsername.Text, " ")
+
+        If username = "" Then
+            err.SetError(txtUsername, "Please enter a valid [Username]")
+            e.Cancel = True
+        ElseIf IsDuplicatedUser(username) Then
+            err.SetError(txtUsername, "Username is duplicated")
+            e.Cancel = True
+        Else
+            err.SetError(txtUsername, Nothing)
+        End If
+    End Sub
+
+    'Check whther txtEmail is empty 
+    Private Sub txtEmail_Validating(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles txtEmail.Validating
+        Dim email As String = If(txtEmail.Text, " ")
+
+        If email = "" Then
+            err.SetError(txtEmail, "Please enter a valid [Email]")
+            e.Cancel = True
+        ElseIf IsDuplicatedEmail(email) Then
+            err.SetError(txtEmail, "This email have been registered by another account")
+            e.Cancel = True
+        Else
+            err.SetError(txtEmail, Nothing)
+        End If
+    End Sub
+    'Check whther txtPassword is empty 
+    Private Sub txtPassword_Validating(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles txtPassword.Validating
+        Dim password As String = If(txtPassword.Text, " ")
+
+        If password = "" Then
+            err.SetError(txtPassword, "Please enter a valid [password]")
+            e.Cancel = True
+        Else
+            err.SetError(txtPassword, Nothing)
+        End If
+    End Sub
+    'Check whether cboRetype is empty 
+    Private Sub txtRetype_Validating(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles txtRetype.Validating
+        Dim confirm As Boolean
+
+        confirm = ValidatePassword(txtPassword.Text, txtRetype.Text)
+        If confirm = False Then
+            err.SetError(txtRetype, "Your password is unmatch!")
+            e.Cancel = True
+
+        Else
+            err.SetError(txtRetype, Nothing)
+        End If
+
+    End Sub
+    'Check whther cboUserType is empty 
+    Private Sub cboUserType_Validating(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles cboUserType.Validating
+        Dim usertype As String = If(cboUserType.Text, " ")
+
+        If usertype = "" Then
+            err.SetError(cboUserType, "Please Select your user type")
+            e.Cancel = True
+        Else
+            err.SetError(cboUserType, Nothing)
+        End If
+    End Sub
+    'Check whther cboCardType is empty 
+    Private Sub cboCardType_Validating(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles cboCardType.Validating
+        Dim cardtype As String = If(cboCardType.Text, " ")
+
+        If cardtype = "" Then
+            err.SetError(cboCardType, "Please Select your card type")
+            e.Cancel = True
+        Else
+            err.SetError(cboCardType, Nothing)
+        End If
+    End Sub
+
+    'Check whether txtCCV is empty 
+    Private Sub txtCCV_Validating(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles txtCCV.Validating
+        Dim ccv As String = If(txtCCV.Text, " ")
+
+        If ccv = "" Then
+            err.SetError(txtCCV, "Please Enter a valid CCV")
+            e.Cancel = True
+        Else
+            err.SetError(txtCCV, Nothing)
+        End If
+    End Sub
+    'Check whther cboCardCompany is empty 
+    Private Sub cboCardCompany_Validating(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles cboCardCompany.Validating
+        Dim cardcompany As String = If(cboCardCompany.Text, " ")
+
+        If cardcompany = "" Then
+            err.SetError(cboCardCompany, "Please Select your Card Company")
+            e.Cancel = True
+        Else
+            err.SetError(cboCardCompany, Nothing)
+        End If
+    End Sub
+
 End Class
