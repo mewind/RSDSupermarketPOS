95b113ade7fd2bf782f6373c82a056cdd0c7b0c0
lkc442012 <lkc442012@hotmail.com>
2016-12-01 23:22:55 +0800

    [#134241075] Added UI design of other forms
    Added icons to each function, mouse leave will become black and white, mouse hover will become color icon.

--- code/Home.vb
+++ code/Home.vb
@@ -72,5 +72,111 @@
         frmUserLogin.ShowDialog(Me)
     End Sub
 
+    '<<<<<<<<<<<<-------------------FORMAT CODE----------------------------------------------------------->>>>>
+
+    'Membership 
+    Private Sub btnSearchMember_MouseHover(sender As Object, e As EventArgs) Handles btnSearchMember.MouseHover
+        'Change to coloured image wheh mouse hover
+        btnSearchMember.Image = My.Resources.search2
+    End Sub
+
+    Private Sub btnViewDetails_MouseHover(sender As Object, e As EventArgs) Handles btnViewDetails.MouseHover
+        'Change to coloured image wheh mouse hover
+        btnViewDetails.Image = My.Resources.view2
+    End Sub
+
+    Private Sub btnRegister_MouseHover(sender As Object, e As EventArgs) Handles btnRegister.MouseHover
+        'Change to coloured image wheh mouse hover
+        btnRegister.Image = My.Resources.add2
+    End Sub
+
+    Private Sub btnViewDetails_MouseLeave(sender As Object, e As EventArgs) Handles btnViewDetails.MouseLeave
+        'Change to sepia image wheh mouse leave
+        btnViewDetails.Image = My.Resources.view1_3
+    End Sub
+
+    Private Sub btnRegister_MouseLeave(sender As Object, e As EventArgs) Handles btnRegister.MouseLeave
+        'Change to sepia image wheh mouse leave
+        btnRegister.Image = My.Resources.add1
+
+    End Sub
+
+    Private Sub btnSearchMember_MouseLeave(sender As Object, e As EventArgs) Handles btnSearchMember.MouseLeave
+        'Change to sepia image wheh mouse leave
+        btnSearchMember.Image = My.Resources.search1
+    End Sub
+
+    'Products
+    Private Sub btnStationary_MouseHover(sender As Object, e As EventArgs) Handles btnStationary.MouseHover
+        'Change to coloured image wheh mouse hover
+        btnStationary.Image = My.Resources.stationary2
+    End Sub
+
+    Private Sub btnGroceries_MouseHover(sender As Object, e As EventArgs) Handles btnGroceries.MouseHover
+        'Change to coloured image wheh mouse hover
+        btnGroceries.Image = My.Resources.groceries2
+    End Sub
+
+    Private Sub btnClothes_MouseHover(sender As Object, e As EventArgs) Handles btnClothes.MouseHover
+        'Change to coloured image wheh mouse hover
+        btnClothes.Image = My.Resources.clothes2
+    End Sub
+
+    Private Sub btnFood_MouseHover(sender As Object, e As EventArgs) Handles btnFood.MouseHover
+        'Change to coloured image wheh mouse hover
+        btnFood.Image = My.Resources.food2
+    End Sub
+
+    Private Sub btnFood_MouseLeave(sender As Object, e As EventArgs) Handles btnFood.MouseLeave
+        'Change to sepia image wheh mouse leave
+        btnFood.Image = My.Resources.food1
+    End Sub
+
+    Private Sub btnClothes_MouseLeave(sender As Object, e As EventArgs) Handles btnClothes.MouseLeave
+        'Change to sepia image wheh mouse leave
+        btnClothes.Image = My.Resources.clothes1
+    End Sub
+
+    Private Sub btnGroceries_MouseLeave(sender As Object, e As EventArgs) Handles btnGroceries.MouseLeave
+        'Change to sepia image wheh mouse leave
+        btnGroceries.Image = My.Resources.groceries1
+    End Sub
+
+    Private Sub btnStationary_MouseLeave(sender As Object, e As EventArgs) Handles btnStationary.MouseLeave
+        'Change to sepia image wheh mouse leave
+        btnStationary.Image = My.Resources.stationary1
+    End Sub
+
+    'Report tab
+    Private Sub btnSalesReport_MouseHover(sender As Object, e As EventArgs) Handles btnSalesReport.MouseHover
+        'Change to coloured image wheh mouse hover
+        btnSalesReport.Image = My.Resources.dollar2
+    End Sub
+
+    Private Sub btnSalesReport_MouseLeave(sender As Object, e As EventArgs) Handles btnSalesReport.MouseLeave
+        'Change to sepia image wheh mouse leave
+        btnSalesReport.Image = My.Resources.dollar1
+    End Sub
+
+    Private Sub btnExceptionReport_MouseHover(sender As Object, e As EventArgs) Handles btnExceptionReport.MouseHover
+        'Change to coloured image wheh mouse hover
+        btnExceptionReport.Image = My.Resources.exception2
+    End Sub
+
+    Private Sub btnExceptionReport_MouseLeave(sender As Object, e As EventArgs) Handles btnExceptionReport.MouseLeave
+        'Change to sepia image wheh mouse leave
+        btnExceptionReport.Image = My.Resources.exception1
+    End Sub
+
+    Private Sub btnSummaryReport_MouseHover(sender As Object, e As EventArgs) Handles btnSummaryReport.MouseHover
+        'Change to coloured image wheh mouse hover
+        btnSummaryReport.Image = My.Resources.summary2
+    End Sub
+
+    Private Sub btnSummaryReport_MouseLeave(sender As Object, e As EventArgs) Handles btnSummaryReport.MouseLeave
+        'Change to sepia image wheh mouse leave
+        btnSummaryReport.Image = My.Resources.summary1
+    End Sub
+    '<<<<<<<<<<<<-------------------End FORMAT CODE----------------------------------------------------------->>>>>
 
 End Class
