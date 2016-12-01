d8b09bb60d6cc7e100aa91137937c1c605857502
huylin25 <hyelin1125@hotmail.com>
2016-12-01 23:45:38 +0800

    [#134240845][#134240855]Added Register Form
    Added User Registration Form UI design with fields.
    Included :
    - UI design 
    - User details fields
    - Credit details fields

---
+++ code/Register.vb
@@ -0,0 +1,26 @@
+﻿Public Class frmRegister
+    'FORMAT
+    Private Sub btnRegister_MouseLeave(sender As Object, e As EventArgs) Handles btnRegister.MouseLeave
+        btnRegister.BackColor = Color.GreenYellow
+        btnRegister.FlatAppearance.BorderColor = Color.Black
+        btnRegister.FlatAppearance.BorderSize = 2
+    End Sub
+
+    'FORMAT
+    Private Sub btnRegister_MouseHover(sender As Object, e As EventArgs) Handles btnRegister.MouseHover
+        btnRegister.BackColor = Color.Chartreuse
+        btnRegister.FlatAppearance.BorderColor = Color.Black
+        btnRegister.FlatAppearance.BorderSize = 2
+    End Sub
+
+    'CANCEL BUTTON
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+
+
+        'Click to return User login page
+        frmUserLogin.Show()
+        Me.Hide()
+    End Sub
+
+
+End Class
