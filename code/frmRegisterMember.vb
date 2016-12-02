5c4b1c4b6bfd37bb8043432443e4ce6f9dadaac6
huylin25 <hyelin1125@hotmail.com>
2016-12-02 23:42:25 +0800

    [#134240841] Added Member Registration Form
    Include: 
    Basic codes of member registration form

---
+++ code/frmRegisterMember.vb
@@ -0,0 +1,27 @@
+﻿Public Class frmRegisterMember
+    Private Sub frmRegisterMember_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim db As New DBDataContext()
+        Dim member As New Member
+
+        lblMemberID.Text = "MEMB00" & (db.Members.Count() + 1).ToString()
+
+        txtName.Clear()
+        txtPhone.Clear()
+        txtName.Focus()
+    End Sub
+
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+
+        txtName.Text = ""
+        radFemale.Checked = False
+        radMale.Checked = False
+        txtPhone.Text = ""
+        txtName.Focus()
+        txtName.SelectAll()
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Me.Close()
+    End Sub
+End Class
